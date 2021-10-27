c78e04d95fe3f9a8ddc2183ed10da17d7260f58e
Archie <34271220+wardar10@users.noreply.github.com>
2021-10-27 15:50:43 +0100

    new employee logic

--- Logic.js
+++ Logic.js
@@ -1,1 +1,19 @@
 console.log("logic.js")
+
+
+function addEmployee() {
+    let niValue = document.getElementById("ni").value;
+    let nameValue = document.getElementById("name").value;
+    let phoneValue = document.getElementById("phone").value;
+    let addressValue = document.getElementById("address").value;
+    let departmentValue = document.getElementById("department").value;
+
+    const newEmployee = {
+        "ninumber": niValue,
+        "fullname": nameValue,
+        "phone": phoneValue,
+        "address": addressValue,
+        "department": departmentValue
+    }
+    console.log(newEmployee)
+}
