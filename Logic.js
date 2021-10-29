0cf10afbc733344c3b806a2aca950014b3110535
Archie <34271220+wardar10@users.noreply.github.com>
2021-10-29 11:55:13 +0100

    updated read me and comments for js

--- Logic.js
+++ Logic.js
@@ -1,29 +1,37 @@
-console.log("logic.js")
+/*
+Author: Archie Schneider Ward;
+Name: Logic.js
+Purpose: Contain the logic and functions for Index.html to work
+Functions: selectEmployee, splitWord, viewEmployee, closeView, showEmployee, editEmployee, clearDropdown, addEmployee, deleteEmployee, filterDepartment, closeFilterView
+Data: employeeInfo from sampleData.js
+*/
+// calls the data from sampleData.js
 var myData = employeeInfo
 
+//employee view buttons
 const formView = document.getElementById('form-view');
 let viewBtn = document.getElementById("viewBtn");
 viewBtn.addEventListener("click", viewEmployee);
 let clearBtn = document.getElementById("clearViewBtn");
 clearBtn.addEventListener("click", closeView);
-
+// employee filter view buttons
 const formFilterView = document.getElementById('form-filter');
 let formFilter = document.getElementById("viewFilterBtn");
 formFilter.addEventListener('click', filterDepartment);
 
-
+// employee add buttons
 const formAdd = document.getElementById("form-addId");
 formAdd.addEventListener('submit', addEmployee);
 
-
-
+// calls the dropdown menu on page load
 selectEmployee()
 
+//retrieves the last word in the string, used to split the name and NI number in the dropdown
 function splitWord(word) {
     var n = word.split(" ");
     return n[n.length - 1]
 }
-
+// creates a table for employees
 function viewEmployee() {
     let table = document.createElement("table");
     let col = [];
@@ -51,20 +59,16 @@ function viewEmployee() {
     divContainer.innerHTML = "";
     divContainer.appendChild(table);
 }
-
+//sets the view element to empty
 function closeView() {
     document.getElementById("employeeData").innerHTML = ""
 }
 
-/*
-Name; showEmployee Parameter: the selected employee
-Purpose; passes the data of the selected employee into the text boxes for editing
-*/
+//passes the data of the selected employee into the text boxes for editing
 function showEmployee(element) {
     var text = element.options[element.selectedIndex].text;
     document.getElementById("employee").innerHTML = text;
     document.getElementById("employee2").innerHTML = text;
-
     var niSelection = splitWord(text)
     for (i = 0; i < myData.length; i++) {
         if (niSelection == myData[i]["ninumber"]) {
@@ -76,14 +80,11 @@ function showEmployee(element) {
         }
     }
 }
-
-
-
+//gets the selected employee and allows the edit of that employee
 function editEmployee() {
     let selectedEmployee = splitWord(document.getElementById("employee2").innerText)
     for (i = 0; i < myData.length; i++) {
         if (selectedEmployee == myData[i].ninumber) {
-            console.log(myData[i])
             let niValue = document.getElementById("niEdit").value;
             let nameValue = document.getElementById("nameEdit").value;
             let phoneValue = document.getElementById("phoneEdit").value;
@@ -102,12 +103,11 @@ function editEmployee() {
     selectEmployee()
     viewEmployee()
 }
-
+//fills in the dropdown elements with the employees names and ni number incase duplicate names
 function selectEmployee() {
     clearDropdown()
     select = document.getElementById("selectEmployee")
     for (let i = 0; i < myData.length; i++) {
-
         var opt2 = myData[i]["ninumber"]
         var opt1 = myData[i]["fullname"]
         var opt = opt1 + "    " + opt2
@@ -117,14 +117,14 @@ function selectEmployee() {
         select.appendChild(el);
     }
 }
-
+// clears the dropdown by removing the elements children (the options)
 function clearDropdown() {
     const myNode = document.getElementById("selectEmployee")
     while (myNode.firstChild) {
         myNode.removeChild(myNode.lastChild)
     }
 }
-
+// adds employee and pushes to the array
 function addEmployee() {
     let niValue = document.getElementById("ni").value;
     let nameValue = document.getElementById("name").value;
@@ -138,7 +138,6 @@ function addEmployee() {
             return;
         }
     }
-
     const newEmployee = {
         "ninumber": niValue,
         "fullname": nameValue,
@@ -147,14 +146,12 @@ function addEmployee() {
         "department": departmentValue
     }
     myData.push(newEmployee)
-    console.log(myData.length)
     selectEmployee();
     viewEmployee()
 }
 
-
+// deletes employee by the ni number
 function deleteEmployee() {
-    console.log("delete employee")
     var selection = document.getElementById('employee2');
     var textSelection = selection.innerText
     var niSelection = splitWord(textSelection)
@@ -167,7 +164,7 @@ function deleteEmployee() {
     }
     selectEmployee()
 }
-
+// creates a view of employees where employees work for department specified 
 function filterDepartment() {
     let department = document.getElementById("departmentSel").value;
     const departmentArray = []
@@ -204,8 +201,7 @@ function filterDepartment() {
     divContainer.innerHTML = "";
     divContainer.appendChild(table);
 }
-
+// closes the filter view
 function closeFilterView() {
-    console.log("close filter")
     document.getElementById("departmentView").innerHTML = ""
 }
--- README.md
+++ README.md
@@ -1,13 +1,12 @@
 # JS_App
 
-####Why are we doing this?
-
-### Cursed
-##How I expected the challenge to go.
-###
-##What went well?
-###
-##What didn't go as planned?
-###
-##Possible improvements for future
-###
+## Why are we doing this?
+#### I have been tasked to create a HR application containing and capturing employee information. This is a CRUD (Create, Read, Update, Delete) application giving an opportunitiy to show and implement the practices learnt during QA training. 
+## How I expected the challenge to go.
+#### Having had limited experience of front-end but a fair amount in javascript, I expected the project not to be too challenging though challenge me in some areas. My main goal of the project was to create an application with no bugs.
+## What went well?
+#### The use of github branches allowed me to revert back to a previous branch/ commit where something went wrong, from there I was able to retrace mysteps and fix the bug that I had created. The use of bootstrap and some extended css to compliment allowed me to create a visually and intuitive application easily instead of the base HTML which, though function, was not pretty.
+## What didn't go as planned?
+#### The dropdown menu for selecting each employee was not updating and when it was, it was adding the entire employee list added ontop of the existing list, so you had two duplicate sets of employees in the dropdown list. This was fixed however by clearing the dropdown elements before the dropdown elements were created again. 
+## Possible improvements for future
+#### The use of React could improve the reusability and creation of Cards where it could offer more customisation in the event of the application function expands.
